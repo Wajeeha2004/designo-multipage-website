5bc0a882e01c17150c846b6c56f5c7386bb14766
Wajeeha Jahangir <waj2004.20@gmail.com>
2025-11-14 19:32:03 +0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+
+document.querySelector('.header__hamburger').addEventListener('click', function() {
+            document.querySelector('.header__nav-links').classList.toggle('header__nav-links--show');
+        });
